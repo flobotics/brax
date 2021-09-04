e34572eccc214f2768108c10e0c23a8949a937d1
fk <inflo@web.de>
2021-09-04 21:23:14 +0200

    fix

--- brax/envs/testenv.py
+++ brax/envs/testenv.py
@@ -20,19 +20,19 @@ class SkeletonEnv(env.Env):
     config = text_format.Parse(_SYSTEM_CONFIG, brax.Config())
     super().__init__(config, **kwargs)
     self.target_idx = self.sys.body_idx['target']
-    self.box_idx = self.sys.body_idx['box_2']
+    self.arm_idx = self.sys.body_idx['box_2']
 
   def reset(self, rng: jnp.ndarray) -> env.State:
     qp = self.sys.default_qp()
     rng, target = self._random_target(rng)
     pos = jax.ops.index_update(qp.pos, jax.ops.index[self.target_idx], target)
     qp = dataclasses.replace(qp, pos=pos)
-    
     info = self.sys.info(qp)
     obs = self._get_obs(qp, info)
     reward, done, steps, zero = jnp.zeros(4)
     metrics = {
-        'hits': zero
+        'rewardDist': zero,
+        'rewardCtrl': zero,
     }
     return env.State(rng, qp, info, obs, reward, done, steps, metrics)
 
@@ -41,32 +41,41 @@ class SkeletonEnv(env.Env):
     qp, info = self.sys.step(state.qp, action)
     obs = self._get_obs(qp, info)
 
-    #print(f"qpos-box >{state.qp.pos[self.box_idx]}<")
-        
-    target_pos = qp.pos[self.target_idx]
-    object_pos = qp.pos[self.box_idx]
-    target_rel = target_pos - object_pos
-    target_dist = jnp.linalg.norm(target_rel)
-    
-    #print(f"reward >{target_dist}<")
-    
-    reward = -target_dist
-
-    #reward = 1.0
+    # vector from tip to target is last 3 entries of obs vector
+    reward_dist = -jnp.linalg.norm(obs[-3:])
+    reward_ctrl = -jnp.square(action).sum()
+    reward = reward_dist + reward_ctrl
 
     steps = state.steps + self.action_repeat
-    
     done = jnp.where(steps >= self.episode_length, 1.0, 0.0)
     metrics = {
-        'hits': reward
+        'rewardDist': reward_dist,
+        'rewardCtrl': reward_ctrl,
     }
 
     return env.State(rng, qp, info, obs, reward, done, steps, metrics)
 
   def _get_obs(self, qp: brax.QP, info: brax.Info) -> jnp.ndarray:
     """Egocentric observation of target and arm body."""
-    
-    return qp.pos[self.target_idx]
+
+    # some pre-processing to pull joint angles and velocities
+    (joint_angle,), _ = self.sys.joint_revolute.angle_vel(qp)
+
+    # qpos:
+    # x,y coord of target
+    qpos = [qp.pos[self.target_idx, :2]]
+
+    # dist to target and speed of tip
+    arm_qps = take(qp, jnp.array(self.arm_idx))
+    tip_pos, tip_vel = math.to_world(arm_qps, jnp.array([0.11, 0., 0.]))
+    tip_to_target = [tip_pos - qp.pos[self.target_idx]]
+    cos_sin_angle = [jnp.cos(joint_angle), jnp.sin(joint_angle)]
+
+    # qvel:
+    # velocity of tip
+    qvel = [tip_vel[:2]]
+
+    return jnp.concatenate(cos_sin_angle + qpos + qvel + tip_to_target)
 
   def _random_target(self, rng: jnp.ndarray) -> Tuple[jnp.ndarray, jnp.ndarray]:
     """Returns a target location in a random circle slightly above xy plane."""
@@ -156,16 +165,15 @@ joints {
       max: 60
   }
   rotation { x: 90 }
-  limit_strength: 100.0
+  limit_strength: 0.0
   spring_damping: 30.0
   stiffness: 1000.0
-  angular_damping: 35
 }
 
 actuators {
   name: "joint0"
   joint: "joint0"
-  strength: 30.0
+  strength: 50.0
   torque {
   }
 }
